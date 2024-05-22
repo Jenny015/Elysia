67331aa3e8d861a10cef8ee761b19507fcaec982
LAU Sze Ting (230268178) <230268178@stu.vtc.edu.hk>
2024-05-22 18:09:25 +0800

    First version

---
+++ Elysia/Elysia/newOrder.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Elysia
+{
+    partial class newOrder
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.lblDept = new System.Windows.Forms.Label();
+            this.imgLogo = new System.Windows.Forms.PictureBox();
+            this.lblDealerID = new System.Windows.Forms.Label();
+            this.cbDealerID = new System.Windows.Forms.ComboBox();
+            this.cbPartID = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(84)))), ((int)(((byte)(129)))));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.radioButton5);
+            this.panel1.Controls.Add(this.radioButton4);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.lblDept);
+            this.panel1.Controls.Add(this.imgLogo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Size = new System.Drawing.Size(419, 1292);
+            this.panel1.TabIndex = 2;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButton5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
+            this.radioButton5.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.radioButton5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton5.Image = global::Elysia.Properties.Resources.logout;
+            this.radioButton5.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.radioButton5.Location = new System.Drawing.Point(4, 1226);
+            this.radioButton5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(409, 60);
+            this.radioButton5.TabIndex = 6;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Logout";
+            this.radioButton5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioButton5.UseVisualStyleBackColor = false;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButton4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
+            this.radioButton4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.radioButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton4.Image = global::Elysia.Properties.Resources.ViewOrder;
+            this.radioButton4.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.radioButton4.Location = new System.Drawing.Point(4, 499);
+            this.radioButton4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(409, 75);
+            this.radioButton4.TabIndex = 5;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "View Order";
+            this.radioButton4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioButton4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.radioButton4.UseVisualStyleBackColor = false;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButton3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
+            this.radioButton3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.radioButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton3.Image = global::Elysia.Properties.Resources.newOrder;
+            this.radioButton3.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.radioButton3.Location = new System.Drawing.Point(4, 424);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(409, 75);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "New Order";
+            this.radioButton3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioButton3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.radioButton3.UseVisualStyleBackColor = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButton2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
+            this.radioButton2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.radioButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton2.Image = global::Elysia.Properties.Resources.ViewDealer;
+            this.radioButton2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.radioButton2.Location = new System.Drawing.Point(4, 349);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(409, 75);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "View Dealer";
+            this.radioButton2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioButton2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.radioButton2.UseVisualStyleBackColor = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
+            this.radioButton1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.radioButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton1.Image = global::Elysia.Properties.Resources.newDealer;
+            this.radioButton1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.radioButton1.Location = new System.Drawing.Point(4, 274);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(409, 75);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "New Dealer";
+            this.radioButton1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.radioButton1.UseVisualStyleBackColor = false;
+            // 
+            // lblDept
+            // 
+            this.lblDept.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblDept.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDept.Location = new System.Drawing.Point(4, 199);
+            this.lblDept.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDept.Name = "lblDept";
+            this.lblDept.Size = new System.Drawing.Size(409, 75);
+            this.lblDept.TabIndex = 1;
+            this.lblDept.Text = "label1";
+            this.lblDept.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // imgLogo
+            // 
+            this.imgLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(84)))), ((int)(((byte)(129)))));
+            this.imgLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.imgLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.imgLogo.Image = global::Elysia.Properties.Resources.logo;
+            this.imgLogo.Location = new System.Drawing.Point(4, 4);
+            this.imgLogo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.imgLogo.Name = "imgLogo";
+            this.imgLogo.Size = new System.Drawing.Size(409, 195);
+            this.imgLogo.TabIndex = 0;
+            this.imgLogo.TabStop = false;
+            // 
+            // lblDealerID
+            // 
+            this.lblDealerID.AutoSize = true;
+            this.lblDealerID.Location = new System.Drawing.Point(534, 153);
+            this.lblDealerID.Name = "lblDealerID";
+            this.lblDealerID.Size = new System.Drawing.Size(73, 18);
+            this.lblDealerID.TabIndex = 3;
+            this.lblDealerID.Text = "DealerID";
+            // 
+            // cbDealerID
+            // 
+            this.cbDealerID.FormattingEnabled = true;
+            this.cbDealerID.Location = new System.Drawing.Point(643, 150);
+            this.cbDealerID.Name = "cbDealerID";
+            this.cbDealerID.Size = new System.Drawing.Size(121, 26);
+            this.cbDealerID.TabIndex = 4;
+            // 
+            // cbPartID
+            // 
+            this.cbPartID.FormattingEnabled = true;
+            this.cbPartID.Location = new System.Drawing.Point(643, 275);
+            this.cbPartID.Name = "cbPartID";
+            this.cbPartID.Size = new System.Drawing.Size(121, 26);
+            this.cbPartID.TabIndex = 5;
+            // 
+            // newOrder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2376, 1292);
+            this.Controls.Add(this.cbPartID);
+            this.Controls.Add(this.cbDealerID);
+            this.Controls.Add(this.lblDealerID);
+            this.Controls.Add(this.panel1);
+            this.Name = "newOrder";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imgLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblDept;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        public System.Windows.Forms.PictureBox imgLogo;
+        private System.Windows.Forms.Label lblDealerID;
+        private System.Windows.Forms.ComboBox cbDealerID;
+        private System.Windows.Forms.ComboBox cbPartID;
+    }
+}
+
---
+++ Elysia/Elysia/newOrder.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Elysia
+{
+    public partial class newOrder : Form
+    {
+        MySqlConnection cnn;
+        public newOrder()
+        {
+            InitializeComponent();
+            cnn = new MySqlConnection("server=localhost;database=elysia;uid=root;pwd=\"\";");
+            loadInformation();
+        }
+
+        public void ConnectToSql()
+        {
+            try
+            {
+                cnn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Fail to connect MySQL\n"+ex.Message);
+            }
+        }
+
+        public void loadInformation()
+        {
+            ConnectToSql();
+            MySqlCommand cmd = cnn.CreateCommand();
+
+            //load dealerID
+            cmd.CommandText = "SELECT dealerID FROM dealer";
+            using (MySqlDataReader reader = cmd.ExecuteReader())
+            {
+                cbDealerID.Items.Clear();
+
+                while (reader.Read())
+                {
+                    cbDealerID.Items.Add(reader.GetString(0));
+                }
+            }
+
+                
+
+            cmd.CommandText = "SELECT partID FROM part";
+            using (MySqlDataReader reader = cmd.ExecuteReader())
+            {
+                cbPartID.Items.Clear();
+
+                while (reader.Read())
+                {
+                    cbPartID.Items.Add(reader.GetString(0));
+                }
+            }
+
+            cnn.Close();
+        }
+
+    }
+}
