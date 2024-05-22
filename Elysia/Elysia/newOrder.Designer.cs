5bfd5de377f06d9df332c47f3d41964920083b6a
LAU Sze Ting <230268178@stu.vtc.edu.hk>
2024-05-22 22:57:11 +0800

    New Order: add, new OrderID, date

--- Elysia/Elysia/newOrder.Designer.cs
+++ Elysia/Elysia/newOrder.Designer.cs
@@ -28,19 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(newOrder));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.lblDept = new System.Windows.Forms.Label();
+            this.lblDealerID = new System.Windows.Forms.Label();
+            this.cbDealerID = new System.Windows.Forms.ComboBox();
+            this.cbPartID = new System.Windows.Forms.ComboBox();
             this.radioButton5 = new System.Windows.Forms.RadioButton();
             this.radioButton4 = new System.Windows.Forms.RadioButton();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.lblDept = new System.Windows.Forms.Label();
             this.imgLogo = new System.Windows.Forms.PictureBox();
-            this.lblDealerID = new System.Windows.Forms.Label();
-            this.cbDealerID = new System.Windows.Forms.ComboBox();
-            this.cbPartID = new System.Windows.Forms.ComboBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lblItem = new System.Windows.Forms.Label();
+            this.nQty = new System.Windows.Forms.NumericUpDown();
+            this.lbItems = new System.Windows.Forms.ListBox();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblOrderID = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.imgLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nQty)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -56,11 +66,50 @@
             this.panel1.Controls.Add(this.imgLogo);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.panel1.Size = new System.Drawing.Size(419, 1292);
+            this.panel1.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.panel1.Size = new System.Drawing.Size(373, 879);
             this.panel1.TabIndex = 2;
+            // 
+            // lblDept
+            // 
+            this.lblDept.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblDept.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDept.Location = new System.Drawing.Point(4, 165);
+            this.lblDept.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDept.Name = "lblDept";
+            this.lblDept.Size = new System.Drawing.Size(363, 62);
+            this.lblDept.TabIndex = 1;
+            this.lblDept.Text = "label1";
+            this.lblDept.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDealerID
+            // 
+            this.lblDealerID.AutoSize = true;
+            this.lblDealerID.Location = new System.Drawing.Point(412, 266);
+            this.lblDealerID.Name = "lblDealerID";
+            this.lblDealerID.Size = new System.Drawing.Size(59, 15);
+            this.lblDealerID.TabIndex = 3;
+            this.lblDealerID.Text = "DealerID";
+            // 
+            // cbDealerID
+            // 
+            this.cbDealerID.FormattingEnabled = true;
+            this.cbDealerID.Location = new System.Drawing.Point(528, 263);
+            this.cbDealerID.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cbDealerID.Name = "cbDealerID";
+            this.cbDealerID.Size = new System.Drawing.Size(108, 23);
+            this.cbDealerID.TabIndex = 4;
+            // 
+            // cbPartID
+            // 
+            this.cbPartID.FormattingEnabled = true;
+            this.cbPartID.Location = new System.Drawing.Point(528, 330);
+            this.cbPartID.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cbPartID.Name = "cbPartID";
+            this.cbPartID.Size = new System.Drawing.Size(108, 23);
+            this.cbPartID.TabIndex = 5;
             // 
             // radioButton5
             // 
@@ -70,10 +119,10 @@
             this.radioButton5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.radioButton5.Image = global::Elysia.Properties.Resources.logout;
             this.radioButton5.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.radioButton5.Location = new System.Drawing.Point(4, 1226);
-            this.radioButton5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton5.Location = new System.Drawing.Point(4, 824);
+            this.radioButton5.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.radioButton5.Name = "radioButton5";
-            this.radioButton5.Size = new System.Drawing.Size(409, 60);
+            this.radioButton5.Size = new System.Drawing.Size(363, 50);
             this.radioButton5.TabIndex = 6;
             this.radioButton5.TabStop = true;
             this.radioButton5.Text = "Logout";
@@ -88,10 +137,10 @@
             this.radioButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.radioButton4.Image = global::Elysia.Properties.Resources.ViewOrder;
             this.radioButton4.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.radioButton4.Location = new System.Drawing.Point(4, 499);
-            this.radioButton4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton4.Location = new System.Drawing.Point(4, 413);
+            this.radioButton4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(409, 75);
+            this.radioButton4.Size = new System.Drawing.Size(363, 62);
             this.radioButton4.TabIndex = 5;
             this.radioButton4.TabStop = true;
             this.radioButton4.Text = "View Order";
@@ -107,10 +156,10 @@
             this.radioButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.radioButton3.Image = global::Elysia.Properties.Resources.newOrder;
             this.radioButton3.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.radioButton3.Location = new System.Drawing.Point(4, 424);
-            this.radioButton3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton3.Location = new System.Drawing.Point(4, 351);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(409, 75);
+            this.radioButton3.Size = new System.Drawing.Size(363, 62);
             this.radioButton3.TabIndex = 4;
             this.radioButton3.TabStop = true;
             this.radioButton3.Text = "New Order";
@@ -126,10 +175,10 @@
             this.radioButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.radioButton2.Image = global::Elysia.Properties.Resources.ViewDealer;
             this.radioButton2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.radioButton2.Location = new System.Drawing.Point(4, 349);
-            this.radioButton2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton2.Location = new System.Drawing.Point(4, 289);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(409, 75);
+            this.radioButton2.Size = new System.Drawing.Size(363, 62);
             this.radioButton2.TabIndex = 3;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "View Dealer";
@@ -145,10 +194,10 @@
             this.radioButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.radioButton1.Image = global::Elysia.Properties.Resources.newDealer;
             this.radioButton1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.radioButton1.Location = new System.Drawing.Point(4, 274);
-            this.radioButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton1.Location = new System.Drawing.Point(4, 227);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(409, 75);
+            this.radioButton1.Size = new System.Drawing.Size(363, 62);
             this.radioButton1.TabIndex = 2;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "New Dealer";
@@ -156,69 +205,130 @@
             this.radioButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.radioButton1.UseVisualStyleBackColor = false;
             // 
-            // lblDept
-            // 
-            this.lblDept.Dock = System.Windows.Forms.DockStyle.Top;
-            this.lblDept.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDept.Location = new System.Drawing.Point(4, 199);
-            this.lblDept.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblDept.Name = "lblDept";
-            this.lblDept.Size = new System.Drawing.Size(409, 75);
-            this.lblDept.TabIndex = 1;
-            this.lblDept.Text = "label1";
-            this.lblDept.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
             // imgLogo
             // 
             this.imgLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(84)))), ((int)(((byte)(129)))));
             this.imgLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.imgLogo.Dock = System.Windows.Forms.DockStyle.Top;
             this.imgLogo.Image = global::Elysia.Properties.Resources.logo;
-            this.imgLogo.Location = new System.Drawing.Point(4, 4);
-            this.imgLogo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.imgLogo.Location = new System.Drawing.Point(4, 3);
+            this.imgLogo.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.imgLogo.Name = "imgLogo";
-            this.imgLogo.Size = new System.Drawing.Size(409, 195);
+            this.imgLogo.Size = new System.Drawing.Size(363, 162);
             this.imgLogo.TabIndex = 0;
             this.imgLogo.TabStop = false;
             // 
-            // lblDealerID
+            // btnAdd
             // 
-            this.lblDealerID.AutoSize = true;
-            this.lblDealerID.Location = new System.Drawing.Point(534, 153);
-            this.lblDealerID.Name = "lblDealerID";
-            this.lblDealerID.Size = new System.Drawing.Size(73, 18);
-            this.lblDealerID.TabIndex = 3;
-            this.lblDealerID.Text = "DealerID";
+            this.btnAdd.Location = new System.Drawing.Point(874, 329);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
-            // cbDealerID
+            // lblItem
             // 
-            this.cbDealerID.FormattingEnabled = true;
-            this.cbDealerID.Location = new System.Drawing.Point(643, 150);
-            this.cbDealerID.Name = "cbDealerID";
-            this.cbDealerID.Size = new System.Drawing.Size(121, 26);
-            this.cbDealerID.TabIndex = 4;
+            this.lblItem.AutoSize = true;
+            this.lblItem.Location = new System.Drawing.Point(412, 454);
+            this.lblItem.Name = "lblItem";
+            this.lblItem.Size = new System.Drawing.Size(38, 15);
+            this.lblItem.TabIndex = 7;
+            this.lblItem.Text = "Items";
             // 
-            // cbPartID
+            // nQty
             // 
-            this.cbPartID.FormattingEnabled = true;
-            this.cbPartID.Location = new System.Drawing.Point(643, 275);
-            this.cbPartID.Name = "cbPartID";
-            this.cbPartID.Size = new System.Drawing.Size(121, 26);
-            this.cbPartID.TabIndex = 5;
+            this.nQty.Location = new System.Drawing.Point(719, 331);
+            this.nQty.Maximum = new decimal(new int[] {
+            -727379969,
+            232,
+            0,
+            0});
+            this.nQty.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nQty.Name = "nQty";
+            this.nQty.Size = new System.Drawing.Size(120, 25);
+            this.nQty.TabIndex = 8;
+            this.nQty.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // lbItems
+            // 
+            this.lbItems.FormattingEnabled = true;
+            this.lbItems.ItemHeight = 15;
+            this.lbItems.Location = new System.Drawing.Point(415, 484);
+            this.lbItems.Name = "lbItems";
+            this.lbItems.Size = new System.Drawing.Size(534, 139);
+            this.lbItems.Sorted = true;
+            this.lbItems.TabIndex = 9;
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(716, 115);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(41, 15);
+            this.lblDate.TabIndex = 10;
+            this.lblDate.Text = "label1";
+            // 
+            // lblOrderID
+            // 
+            this.lblOrderID.AutoSize = true;
+            this.lblOrderID.Location = new System.Drawing.Point(525, 115);
+            this.lblOrderID.Name = "lblOrderID";
+            this.lblOrderID.Size = new System.Drawing.Size(41, 15);
+            this.lblOrderID.TabIndex = 11;
+            this.lblOrderID.Text = "label1";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(796, 772);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(939, 772);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 13;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
             // 
             // newOrder
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2376, 1292);
+            this.ClientSize = new System.Drawing.Size(1710, 879);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblOrderID);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lbItems);
+            this.Controls.Add(this.nQty);
+            this.Controls.Add(this.lblItem);
+            this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.cbPartID);
             this.Controls.Add(this.cbDealerID);
             this.Controls.Add(this.lblDealerID);
             this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "newOrder";
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.imgLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nQty)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -237,6 +347,14 @@
         private System.Windows.Forms.Label lblDealerID;
         private System.Windows.Forms.ComboBox cbDealerID;
         private System.Windows.Forms.ComboBox cbPartID;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lblItem;
+        private System.Windows.Forms.NumericUpDown nQty;
+        private System.Windows.Forms.ListBox lbItems;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblOrderID;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSubmit;
     }
 }
 
--- Elysia/Elysia/newOrder.cs
+++ Elysia/Elysia/newOrder.cs
@@ -14,6 +14,8 @@ namespace Elysia
     public partial class newOrder : Form
     {
         MySqlConnection cnn;
+        Dictionary<String, int> orderParts = new Dictionary<string, int>();
+        String newOrderID;
         public newOrder()
         {
             InitializeComponent();
@@ -38,7 +40,7 @@ namespace Elysia
             ConnectToSql();
             MySqlCommand cmd = cnn.CreateCommand();
 
-            //load dealerID
+            //load dealerIDs
             cmd.CommandText = "SELECT dealerID FROM dealer";
             using (MySqlDataReader reader = cmd.ExecuteReader())
             {
@@ -49,9 +51,7 @@ namespace Elysia
                     cbDealerID.Items.Add(reader.GetString(0));
                 }
             }
-
-                
-
+            //load partIDs
             cmd.CommandText = "SELECT partID FROM part";
             using (MySqlDataReader reader = cmd.ExecuteReader())
             {
@@ -63,8 +63,59 @@ namespace Elysia
                 }
             }
 
+            cmd.CommandText = "SELECT MAX(OrderID) FROM `order`";
+            using (MySqlDataReader reader = cmd.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    var maxOrderID = reader[0];
+                    if (maxOrderID != null && maxOrderID != DBNull.Value)
+                    {
+                        var numberPart = int.Parse(maxOrderID.ToString().Substring(1));
+                        var newNumber = numberPart + 1;
+                        newOrderID = $"N{newNumber:D9}";
+                        lblOrderID.Text = newOrderID;
+                    }
+                }
+                
+            }
+
             cnn.Close();
+
+            //display date of the order
+            lblDate.Text = DateTime.Today.ToString("d");
+
         }
 
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if(cbPartID.SelectedItem != null && nQty.Value > 0)
+            {
+                String addPart = (String)cbPartID.SelectedItem;
+                int qty = Decimal.ToInt32(nQty.Value);
+                bool exist = false;
+                foreach (var parts in orderParts)
+                {
+                    if(parts.Key == addPart)
+                    {
+                        lbItems.Items.Remove(addPart + " (" + parts.Value + ")");
+                        orderParts[addPart] += qty;
+                        lbItems.Items.Add(addPart + " (" + orderParts[addPart] + ")");
+                        exist = true;
+                        break;
+                    }
+                }
+                if (!exist)
+                {
+                    orderParts.Add(addPart, qty);
+                    lbItems.Items.Add(addPart + " (" + qty + ")");
+                }
+
+                cbPartID.SelectedItem = null;
+                nQty.Value = 1;
+                cbPartID.Focus();
+
+            }
+        }
     }
 }
