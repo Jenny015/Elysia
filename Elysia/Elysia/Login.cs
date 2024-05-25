7e322934a53d3541a632e9f1a3d8a9101ba0e9bb
CHONG Fung Yan (230113181) <230113181@stu.vtc.edu.hk>
2024-05-25 16:52:58 +0800

    log in& side bar

--- Elysia/Elysia/Login.cs
+++ Elysia/Elysia/Login.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using Mysqlx.Crud;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -25,6 +27,80 @@ namespace Elysia
             tb_password.AutoSize = false;
             tb_username.Size = new System.Drawing.Size(250, 30);
             tb_password.Size = new System.Drawing.Size(250, 30);
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            string username = tb_username.Text;
+            string password = tb_password.Text;
+
+            using (MySqlConnection connection = new MySqlConnection("server=localhost;database=elysia;uid=root;pwd=password;"))
+            {
+                connection.Open();
+                MySqlCommand command = new MySqlCommand("SELECT deptID FROM emp WHERE empID = @username AND empPasswd = @password", connection);
+                command.Parameters.AddWithValue("@username", username);
+                command.Parameters.AddWithValue("@password", password);
+
+                string deptID = (string)command.ExecuteScalar();
+
+                if (deptID != null)
+                {
+                    // Login successful, redirect to appropriate department-specific UI
+                    RedirectToDepartmentUI(deptID);
+                }
+                else
+                {
+                    // Login failed, display error message
+                    MessageBox.Show("Invalid username or password.");
+                }
+            }
+        }
+
+        private void RedirectToDepartmentUI(string deptID)
+        {
+            this.Hide();
+            switch (deptID)
+            {
+                case "SO":
+                    NewDealer NewDealer = new NewDealer();
+                    NewDealer.Show();
+                    break;
+                case "SP":
+                    viewDID viewDID = new viewDID();
+                    viewDID.Show();
+                    break;
+                case "SD":
+                    InvoiceGenerateAndDelivery invoiceGenerateAndDelivery = new InvoiceGenerateAndDelivery();
+                    invoiceGenerateAndDelivery.Show();
+                    break;
+                case "IS":
+                    ViewInvoice viewInvoice = new ViewInvoice();
+                    viewInvoice.Show();
+                    break;
+                case "SR":
+                    ScrapItem scrapItem = new ScrapItem();
+                    scrapItem.Show();
+                    break;
+                case "PD":
+                    Reorder reorder = new Reorder();
+                    reorder.Show();
+                    break;
+                case "GI":
+                    GoodsInward goodsInward = new GoodsInward();
+                    goodsInward.Show();
+                    break;
+                case "HR":
+                    NewEmployee newEmployee = new NewEmployee();
+                    newEmployee.Show();
+                    break;
+                case "AC":
+                    ViewLog viewLog = new ViewLog();
+                    viewLog.Show();
+                    break;
+                default:
+                    MessageBox.Show("Invalid department ID.");
+                    break;
+            }
         }
     }
 }
---
+++ Elysia/Elysia/Reorder.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Elysia
+{
+    partial class Reorder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnViewReorder = new System.Windows.Forms.RadioButton();
+            this.btnReorder = new System.Windows.Forms.RadioButton();
+            this.btnViewSupplier = new System.Windows.Forms.RadioButton();
+            this.btnAddSupplier = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.btnLogout = new System.Windows.Forms.RadioButton();
+            this.lblDept = new System.Windows.Forms.Label();
+            this.imgLogo = new System.Windows.Forms.PictureBox();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(419, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1504, 90);
+            this.panel2.TabIndex = 31;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Georgia", 18F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(40, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(188, 41);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Re-order";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(84)))), ((int)(((byte)(129)))));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnViewReorder);
+            this.panel1.Controls.Add(this.btnReorder);
+            this.panel1.Controls.Add(this.btnViewSupplier);
+            this.panel1.Controls.Add(this.btnAddSupplier);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.btnLogout);
+            this.panel1.Controls.Add(this.lblDept);
+            this.panel1.Controls.Add(this.imgLogo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(4);
+            this.panel1.Size = new System.Drawing.Size(419, 1055);
+            this.panel1.TabIndex = 30;
+            // 
+            // btnViewReorder
+            // 
+            this.btnViewReorder.Appearance = System.Windows.Forms.Appearance.Button;
+            this.btnViewReorder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
+            this.btnViewReorder.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnViewReorder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnViewReorder.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.btnViewReorder.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnViewReorder.Location = new System.Drawing.Point(4, 573);
+            this.btnViewReorder.Margin = new System.Windows.Forms.Padding(4);
+            this.btnViewReorder.Name = "btnViewReorder";
+            this.btnViewReorder.Size = new System.Drawing.Size(409, 75);
+            this.btnViewReorder.TabIndex = 11;
+            this.btnViewReorder.TabStop = true;
+            this.btnViewReorder.Text = "View Re-order";
+            this.btnViewReorder.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnViewReorder.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnViewReorder.UseVisualStyleBackColor = false;
+            // 
+            // btnReorder
+            // 
+            this.btnReorder.Appearance = System.Windows.Forms.Appearance.Button;
+            this.btnReorder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
+            this.btnReorder.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnReorder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReorder.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.btnReorder.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnReorder.Location = new System.Drawing.Point(4, 498);
+            this.btnReorder.Margin = new System.Windows.Forms.Padding(4);
+            this.btnReorder.Name = "btnReorder";
+            this.btnReorder.Size = new System.Drawing.Size(409, 75);
+            this.btnReorder.TabIndex = 10;
+            this.btnReorder.TabStop = true;
+            this.btnReorder.Text = "Re-Order";
+            this.btnReorder.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnReorder.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnReorder.UseVisualStyleBackColor = false;
+            // 
+            // btnViewSupplier
+            // 
+            this.btnViewSupplier.Appearance = System.Windows.Forms.Appearance.Button;
+            this.btnViewSupplier.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
+            this.btnViewSupplier.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnViewSupplier.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnViewSupplier.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.btnViewSupplier.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnViewSupplier.Location = new System.Drawing.Point(4, 423);
+            this.btnViewSupplier.Margin = new System.Windows.Forms.Padding(4);
+            this.btnViewSupplier.Name = "btnViewSupplier";
+            this.btnViewSupplier.Size = new System.Drawing.Size(409, 75);
+            this.btnViewSupplier.TabIndex = 9;
+            this.btnViewSupplier.TabStop = true;
+            this.btnViewSupplier.Text = "View Supplier";
+            this.btnViewSupplier.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnViewSupplier.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnViewSupplier.UseVisualStyleBackColor = false;
+            // 
+            // btnAddSupplier
+            // 
+            this.btnAddSupplier.Appearance = System.Windows.Forms.Appearance.Button;
+            this.btnAddSupplier.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
+            this.btnAddSupplier.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAddSupplier.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddSupplier.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.btnAddSupplier.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnAddSupplier.Location = new System.Drawing.Point(4, 348);
+            this.btnAddSupplier.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAddSupplier.Name = "btnAddSupplier";
+            this.btnAddSupplier.Size = new System.Drawing.Size(409, 75);
+            this.btnAddSupplier.TabIndex = 8;
+            this.btnAddSupplier.TabStop = true;
+            this.btnAddSupplier.Text = "Add Supplier";
+            this.btnAddSupplier.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnAddSupplier.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnAddSupplier.UseVisualStyleBackColor = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
+            this.radioButton1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.radioButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.radioButton1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.radioButton1.Location = new System.Drawing.Point(4, 273);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(409, 75);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "User";
+            this.radioButton1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.radioButton1.UseVisualStyleBackColor = false;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Appearance = System.Windows.Forms.Appearance.Button;
+            this.btnLogout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
+            this.btnLogout.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogout.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.btnLogout.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnLogout.Location = new System.Drawing.Point(4, 990);
+            this.btnLogout.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(409, 59);
+            this.btnLogout.TabIndex = 6;
+            this.btnLogout.TabStop = true;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnLogout.UseVisualStyleBackColor = false;
+            // 
+            // lblDept
+            // 
+            this.lblDept.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblDept.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold);
+            this.lblDept.Location = new System.Drawing.Point(4, 198);
+            this.lblDept.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDept.Name = "lblDept";
+            this.lblDept.Size = new System.Drawing.Size(409, 75);
+            this.lblDept.TabIndex = 1;
+            this.lblDept.Text = "Purchasing Department";
+            this.lblDept.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // imgLogo
+            // 
+            this.imgLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(84)))), ((int)(((byte)(129)))));
+            this.imgLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.imgLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.imgLogo.Location = new System.Drawing.Point(4, 4);
+            this.imgLogo.Margin = new System.Windows.Forms.Padding(4);
+            this.imgLogo.Name = "imgLogo";
+            this.imgLogo.Size = new System.Drawing.Size(409, 194);
+            this.imgLogo.TabIndex = 0;
+            this.imgLogo.TabStop = false;
+            // 
+            // Reorder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1923, 1055);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Reorder";
+            this.Text = "Reorder";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imgLogo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton btnViewReorder;
+        private System.Windows.Forms.RadioButton btnReorder;
+        private System.Windows.Forms.RadioButton btnViewSupplier;
+        private System.Windows.Forms.RadioButton btnAddSupplier;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton btnLogout;
+        private System.Windows.Forms.Label lblDept;
+        public System.Windows.Forms.PictureBox imgLogo;
+    }
+}
