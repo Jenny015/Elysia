c147703c397b8b54feb908b6b497a7955262d68a
CHAN Yuet Yi (230268025) <230268025@stu.vtc.edu.hk>
2024-05-25 13:37:39 +0800

    if for  DID

--- Elysia/Elysia/viewDID.cs
+++ Elysia/Elysia/viewDID.cs
@@ -17,11 +17,13 @@ namespace Elysia
         {
             InitializeComponent();
             setDataGridView();
+            this.dataGridView1.CellClick += new DataGridViewCellEventHandler(dataGridView1_CellClick);
+
         }
         private void setDataGridView()
         {
             string connectionString = "server=localhost;database=elysia;user=root;password=\"\"";
-            string query = "SELECT * FROM orderpart";
+            string query = "SELECT * FROM orderpart WHERE opStatus = 'Processing'";
 
             using (MySqlConnection conn = new MySqlConnection(connectionString))
             {
@@ -30,6 +32,37 @@ namespace Elysia
                     DataSet ds = new DataSet();
                     adapter.Fill(ds);
                     dataGridView1.DataSource = ds.Tables[0];
+                    dataGridView1.Columns[0].ReadOnly = true;
+                    dataGridView1.Columns[1].ReadOnly = true;
+
+
+                    DataGridViewButtonColumn buttonColumn = new DataGridViewButtonColumn();
+                    buttonColumn.HeaderText = "Assemble";
+                    buttonColumn.Name = "buttonColumn";
+                    buttonColumn.Text = "Assemble";
+                    buttonColumn.UseColumnTextForButtonValue = true; // This will set the button text to "Click Me"
+
+                    // Add the button column to the DataGridView
+                    dataGridView1.Columns.Add(buttonColumn);
+                }
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Check if the click is on the button column
+            if (e.ColumnIndex == dataGridView1.Columns["buttonColumn"].Index && e.RowIndex >= 0)
+            {
+
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                String actDespQtyData = row.Cells["actDespQty"].Value.ToString();
+
+                // Check if the 'actDespQty' data is not null
+                if (actDespQtyData == "")
+                {
+                    // Perform the action you want to take when the button is clicked
+                    MessageBox.Show("The 'actDespQty' value is NULL.");
+
                 }
             }
         }
