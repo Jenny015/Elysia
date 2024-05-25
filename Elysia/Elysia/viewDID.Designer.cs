896ee22eaf962b471eb6a6119f091cede9e22e6f
CHAN Yuet Yi (230268025) <230268025@stu.vtc.edu.hk>
2024-05-25 12:06:08 +0800

    viewDID

---
+++ Elysia/Elysia/viewDID.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Elysia
+{
+    partial class viewDID
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnLogout = new System.Windows.Forms.RadioButton();
+            this.btnViewOrder = new System.Windows.Forms.RadioButton();
+            this.btnNewOrder = new System.Windows.Forms.RadioButton();
+            this.btnViewDealer = new System.Windows.Forms.RadioButton();
+            this.btnNewDealer = new System.Windows.Forms.RadioButton();
+            this.lblDept = new System.Windows.Forms.Label();
+            this.imgLogo = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgLogo)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(84)))), ((int)(((byte)(129)))));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnLogout);
+            this.panel1.Controls.Add(this.btnViewOrder);
+            this.panel1.Controls.Add(this.btnNewOrder);
+            this.panel1.Controls.Add(this.btnViewDealer);
+            this.panel1.Controls.Add(this.btnNewDealer);
+            this.panel1.Controls.Add(this.lblDept);
+            this.panel1.Controls.Add(this.imgLogo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(4);
+            this.panel1.Size = new System.Drawing.Size(419, 1055);
+            this.panel1.TabIndex = 3;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Appearance = System.Windows.Forms.Appearance.Button;
+            this.btnLogout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
+            this.btnLogout.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogout.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.btnLogout.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnLogout.Location = new System.Drawing.Point(4, 990);
+            this.btnLogout.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(409, 59);
+            this.btnLogout.TabIndex = 6;
+            this.btnLogout.TabStop = true;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnLogout.UseVisualStyleBackColor = false;
+            // 
+            // btnViewOrder
+            // 
+            this.btnViewOrder.Appearance = System.Windows.Forms.Appearance.Button;
+            this.btnViewOrder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
+            this.btnViewOrder.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnViewOrder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnViewOrder.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.btnViewOrder.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnViewOrder.Location = new System.Drawing.Point(4, 498);
+            this.btnViewOrder.Margin = new System.Windows.Forms.Padding(4);
+            this.btnViewOrder.Name = "btnViewOrder";
+            this.btnViewOrder.Size = new System.Drawing.Size(409, 75);
+            this.btnViewOrder.TabIndex = 5;
+            this.btnViewOrder.TabStop = true;
+            this.btnViewOrder.Text = "Late Delivery";
+            this.btnViewOrder.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnViewOrder.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnViewOrder.UseVisualStyleBackColor = false;
+            // 
+            // btnNewOrder
+            // 
+            this.btnNewOrder.Appearance = System.Windows.Forms.Appearance.Button;
+            this.btnNewOrder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
+            this.btnNewOrder.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnNewOrder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNewOrder.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.btnNewOrder.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnNewOrder.Location = new System.Drawing.Point(4, 423);
+            this.btnNewOrder.Margin = new System.Windows.Forms.Padding(4);
+            this.btnNewOrder.Name = "btnNewOrder";
+            this.btnNewOrder.Size = new System.Drawing.Size(409, 75);
+            this.btnNewOrder.TabIndex = 4;
+            this.btnNewOrder.TabStop = true;
+            this.btnNewOrder.Text = "Spare Part";
+            this.btnNewOrder.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnNewOrder.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnNewOrder.UseVisualStyleBackColor = false;
+            // 
+            // btnViewDealer
+            // 
+            this.btnViewDealer.Appearance = System.Windows.Forms.Appearance.Button;
+            this.btnViewDealer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
+            this.btnViewDealer.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnViewDealer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnViewDealer.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.btnViewDealer.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnViewDealer.Location = new System.Drawing.Point(4, 348);
+            this.btnViewDealer.Margin = new System.Windows.Forms.Padding(4);
+            this.btnViewDealer.Name = "btnViewDealer";
+            this.btnViewDealer.Size = new System.Drawing.Size(409, 75);
+            this.btnViewDealer.TabIndex = 3;
+            this.btnViewDealer.TabStop = true;
+            this.btnViewDealer.Text = "DID";
+            this.btnViewDealer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnViewDealer.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnViewDealer.UseVisualStyleBackColor = false;
+            // 
+            // btnNewDealer
+            // 
+            this.btnNewDealer.Appearance = System.Windows.Forms.Appearance.Button;
+            this.btnNewDealer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
+            this.btnNewDealer.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnNewDealer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNewDealer.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.btnNewDealer.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnNewDealer.Location = new System.Drawing.Point(4, 273);
+            this.btnNewDealer.Margin = new System.Windows.Forms.Padding(4);
+            this.btnNewDealer.Name = "btnNewDealer";
+            this.btnNewDealer.Size = new System.Drawing.Size(409, 75);
+            this.btnNewDealer.TabIndex = 2;
+            this.btnNewDealer.TabStop = true;
+            this.btnNewDealer.Text = "User";
+            this.btnNewDealer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnNewDealer.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnNewDealer.UseVisualStyleBackColor = false;
+            // 
+            // lblDept
+            // 
+            this.lblDept.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblDept.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold);
+            this.lblDept.Location = new System.Drawing.Point(4, 198);
+            this.lblDept.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDept.Name = "lblDept";
+            this.lblDept.Size = new System.Drawing.Size(409, 75);
+            this.lblDept.TabIndex = 1;
+            this.lblDept.Text = "Storemen";
+            this.lblDept.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // imgLogo
+            // 
+            this.imgLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(84)))), ((int)(((byte)(129)))));
+            this.imgLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.imgLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.imgLogo.Location = new System.Drawing.Point(4, 4);
+            this.imgLogo.Margin = new System.Windows.Forms.Padding(4);
+            this.imgLogo.Name = "imgLogo";
+            this.imgLogo.Size = new System.Drawing.Size(409, 194);
+            this.imgLogo.TabIndex = 0;
+            this.imgLogo.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(419, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1504, 90);
+            this.panel2.TabIndex = 24;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Georgia", 18F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(40, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(787, 41);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Order Processing System (Assemble DID)";
+            // 
+            // panel3
+            // 
+            this.panel3.AutoSize = true;
+            this.panel3.Controls.Add(this.button4);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(419, 90);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1504, 46);
+            this.panel3.TabIndex = 25;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold);
+            this.button4.Location = new System.Drawing.Point(704, 1);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 41);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Filter";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(420, 142);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 31;
+            this.dataGridView1.Size = new System.Drawing.Size(1503, 913);
+            this.dataGridView1.TabIndex = 26;
+            // 
+            // viewDID
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1923, 1055);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "viewDID";
+            this.Text = "viewDID";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imgLogo)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton btnLogout;
+        private System.Windows.Forms.RadioButton btnViewOrder;
+        private System.Windows.Forms.RadioButton btnNewOrder;
+        private System.Windows.Forms.RadioButton btnViewDealer;
+        private System.Windows.Forms.RadioButton btnNewDealer;
+        private System.Windows.Forms.Label lblDept;
+        public System.Windows.Forms.PictureBox imgLogo;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ Elysia/Elysia/viewDID.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Elysia
+{
+    public partial class viewDID : Form
+    {
+        public viewDID()
+        {
+            InitializeComponent();
+            setDataGridView();
+        }
+        private void setDataGridView()
+        {
+            string connectionString = "server=localhost;database=elysia;user=root;password=\"\"";
+            string query = "SELECT * FROM orderpart";
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                using (MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn))
+                {
+                    DataSet ds = new DataSet();
+                    adapter.Fill(ds);
+                    dataGridView1.DataSource = ds.Tables[0];
+                }
+            }
+        }
+    }
+}
