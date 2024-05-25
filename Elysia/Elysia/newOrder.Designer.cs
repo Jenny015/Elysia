05f4879d2ddaf11053be89d8e08f30643e4bcb63
LAU Sze Ting (230268178) <230268178@stu.vtc.edu.hk>
2024-05-25 13:38:03 +0800

    update Database

--- Elysia/Elysia/newOrder.Designer.cs
+++ Elysia/Elysia/newOrder.Designer.cs
@@ -29,11 +29,8 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.btnLogout = new System.Windows.Forms.RadioButton();
-            this.btnViewOrder = new System.Windows.Forms.RadioButton();
-            this.btnNewOrder = new System.Windows.Forms.RadioButton();
-            this.btnViewDealer = new System.Windows.Forms.RadioButton();
-            this.btnNewDealer = new System.Windows.Forms.RadioButton();
             this.lblDept = new System.Windows.Forms.Label();
             this.imgLogo = new System.Windows.Forms.PictureBox();
             this.cbDealerID = new System.Windows.Forms.ComboBox();
@@ -64,6 +61,10 @@
             this.label8 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.btnNewDealer = new System.Windows.Forms.RadioButton();
+            this.btnViewDealer = new System.Windows.Forms.RadioButton();
+            this.btnNewOrder = new System.Windows.Forms.RadioButton();
+            this.btnViewOrder = new System.Windows.Forms.RadioButton();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.imgLogo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nQty)).BeginInit();
@@ -76,11 +77,12 @@
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(84)))), ((int)(((byte)(129)))));
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Controls.Add(this.btnLogout);
             this.panel1.Controls.Add(this.btnViewOrder);
             this.panel1.Controls.Add(this.btnNewOrder);
             this.panel1.Controls.Add(this.btnViewDealer);
             this.panel1.Controls.Add(this.btnNewDealer);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.btnLogout);
             this.panel1.Controls.Add(this.lblDept);
             this.panel1.Controls.Add(this.imgLogo);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
@@ -90,6 +92,25 @@
             this.panel1.Padding = new System.Windows.Forms.Padding(4);
             this.panel1.Size = new System.Drawing.Size(419, 1055);
             this.panel1.TabIndex = 2;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
+            this.radioButton1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.radioButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.radioButton1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.radioButton1.Location = new System.Drawing.Point(4, 273);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(409, 75);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "User";
+            this.radioButton1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.radioButton1.UseVisualStyleBackColor = false;
             // 
             // btnLogout
             // 
@@ -108,82 +129,6 @@
             this.btnLogout.Text = "Logout";
             this.btnLogout.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.btnLogout.UseVisualStyleBackColor = false;
-            // 
-            // btnViewOrder
-            // 
-            this.btnViewOrder.Appearance = System.Windows.Forms.Appearance.Button;
-            this.btnViewOrder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
-            this.btnViewOrder.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnViewOrder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnViewOrder.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
-            this.btnViewOrder.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnViewOrder.Location = new System.Drawing.Point(4, 498);
-            this.btnViewOrder.Margin = new System.Windows.Forms.Padding(4);
-            this.btnViewOrder.Name = "btnViewOrder";
-            this.btnViewOrder.Size = new System.Drawing.Size(409, 75);
-            this.btnViewOrder.TabIndex = 5;
-            this.btnViewOrder.TabStop = true;
-            this.btnViewOrder.Text = "View Order";
-            this.btnViewOrder.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.btnViewOrder.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.btnViewOrder.UseVisualStyleBackColor = false;
-            // 
-            // btnNewOrder
-            // 
-            this.btnNewOrder.Appearance = System.Windows.Forms.Appearance.Button;
-            this.btnNewOrder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
-            this.btnNewOrder.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnNewOrder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnNewOrder.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
-            this.btnNewOrder.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnNewOrder.Location = new System.Drawing.Point(4, 423);
-            this.btnNewOrder.Margin = new System.Windows.Forms.Padding(4);
-            this.btnNewOrder.Name = "btnNewOrder";
-            this.btnNewOrder.Size = new System.Drawing.Size(409, 75);
-            this.btnNewOrder.TabIndex = 4;
-            this.btnNewOrder.TabStop = true;
-            this.btnNewOrder.Text = "New Order";
-            this.btnNewOrder.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.btnNewOrder.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.btnNewOrder.UseVisualStyleBackColor = false;
-            // 
-            // btnViewDealer
-            // 
-            this.btnViewDealer.Appearance = System.Windows.Forms.Appearance.Button;
-            this.btnViewDealer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
-            this.btnViewDealer.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnViewDealer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnViewDealer.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
-            this.btnViewDealer.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnViewDealer.Location = new System.Drawing.Point(4, 348);
-            this.btnViewDealer.Margin = new System.Windows.Forms.Padding(4);
-            this.btnViewDealer.Name = "btnViewDealer";
-            this.btnViewDealer.Size = new System.Drawing.Size(409, 75);
-            this.btnViewDealer.TabIndex = 3;
-            this.btnViewDealer.TabStop = true;
-            this.btnViewDealer.Text = "View Dealer";
-            this.btnViewDealer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.btnViewDealer.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.btnViewDealer.UseVisualStyleBackColor = false;
-            // 
-            // btnNewDealer
-            // 
-            this.btnNewDealer.Appearance = System.Windows.Forms.Appearance.Button;
-            this.btnNewDealer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
-            this.btnNewDealer.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnNewDealer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnNewDealer.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
-            this.btnNewDealer.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnNewDealer.Location = new System.Drawing.Point(4, 273);
-            this.btnNewDealer.Margin = new System.Windows.Forms.Padding(4);
-            this.btnNewDealer.Name = "btnNewDealer";
-            this.btnNewDealer.Size = new System.Drawing.Size(409, 75);
-            this.btnNewDealer.TabIndex = 2;
-            this.btnNewDealer.TabStop = true;
-            this.btnNewDealer.Text = "New Dealer";
-            this.btnNewDealer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.btnNewDealer.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.btnNewDealer.UseVisualStyleBackColor = false;
             // 
             // lblDept
             // 
@@ -543,6 +488,82 @@
             this.panel2.Size = new System.Drawing.Size(1504, 90);
             this.panel2.TabIndex = 23;
             // 
+            // btnNewDealer
+            // 
+            this.btnNewDealer.Appearance = System.Windows.Forms.Appearance.Button;
+            this.btnNewDealer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
+            this.btnNewDealer.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnNewDealer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNewDealer.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.btnNewDealer.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnNewDealer.Location = new System.Drawing.Point(4, 348);
+            this.btnNewDealer.Margin = new System.Windows.Forms.Padding(4);
+            this.btnNewDealer.Name = "btnNewDealer";
+            this.btnNewDealer.Size = new System.Drawing.Size(409, 75);
+            this.btnNewDealer.TabIndex = 8;
+            this.btnNewDealer.TabStop = true;
+            this.btnNewDealer.Text = "New Dealer";
+            this.btnNewDealer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnNewDealer.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnNewDealer.UseVisualStyleBackColor = false;
+            // 
+            // btnViewDealer
+            // 
+            this.btnViewDealer.Appearance = System.Windows.Forms.Appearance.Button;
+            this.btnViewDealer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
+            this.btnViewDealer.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnViewDealer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnViewDealer.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.btnViewDealer.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnViewDealer.Location = new System.Drawing.Point(4, 423);
+            this.btnViewDealer.Margin = new System.Windows.Forms.Padding(4);
+            this.btnViewDealer.Name = "btnViewDealer";
+            this.btnViewDealer.Size = new System.Drawing.Size(409, 75);
+            this.btnViewDealer.TabIndex = 9;
+            this.btnViewDealer.TabStop = true;
+            this.btnViewDealer.Text = "View Dealer";
+            this.btnViewDealer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnViewDealer.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnViewDealer.UseVisualStyleBackColor = false;
+            // 
+            // btnNewOrder
+            // 
+            this.btnNewOrder.Appearance = System.Windows.Forms.Appearance.Button;
+            this.btnNewOrder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
+            this.btnNewOrder.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnNewOrder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNewOrder.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.btnNewOrder.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnNewOrder.Location = new System.Drawing.Point(4, 498);
+            this.btnNewOrder.Margin = new System.Windows.Forms.Padding(4);
+            this.btnNewOrder.Name = "btnNewOrder";
+            this.btnNewOrder.Size = new System.Drawing.Size(409, 75);
+            this.btnNewOrder.TabIndex = 10;
+            this.btnNewOrder.TabStop = true;
+            this.btnNewOrder.Text = "New Order";
+            this.btnNewOrder.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnNewOrder.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnNewOrder.UseVisualStyleBackColor = false;
+            // 
+            // btnViewOrder
+            // 
+            this.btnViewOrder.Appearance = System.Windows.Forms.Appearance.Button;
+            this.btnViewOrder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
+            this.btnViewOrder.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnViewOrder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnViewOrder.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.btnViewOrder.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnViewOrder.Location = new System.Drawing.Point(4, 573);
+            this.btnViewOrder.Margin = new System.Windows.Forms.Padding(4);
+            this.btnViewOrder.Name = "btnViewOrder";
+            this.btnViewOrder.Size = new System.Drawing.Size(409, 75);
+            this.btnViewOrder.TabIndex = 11;
+            this.btnViewOrder.TabStop = true;
+            this.btnViewOrder.Text = "View Order";
+            this.btnViewOrder.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnViewOrder.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnViewOrder.UseVisualStyleBackColor = false;
+            // 
             // newOrder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
@@ -575,10 +596,6 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label lblDept;
         private System.Windows.Forms.RadioButton btnLogout;
-        private System.Windows.Forms.RadioButton btnViewOrder;
-        private System.Windows.Forms.RadioButton btnNewOrder;
-        private System.Windows.Forms.RadioButton btnViewDealer;
-        private System.Windows.Forms.RadioButton btnNewDealer;
         public System.Windows.Forms.PictureBox imgLogo;
         private System.Windows.Forms.ComboBox cbDealerID;
         private System.Windows.Forms.ComboBox cbPartID;
@@ -608,6 +625,11 @@
         private System.Windows.Forms.Label lblpartName;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton btnViewOrder;
+        private System.Windows.Forms.RadioButton btnNewOrder;
+        private System.Windows.Forms.RadioButton btnViewDealer;
+        private System.Windows.Forms.RadioButton btnNewDealer;
     }
 }
 
