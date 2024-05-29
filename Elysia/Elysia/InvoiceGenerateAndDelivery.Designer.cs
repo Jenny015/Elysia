f8f2f27c84435d411db8507ebb5faa4d2548ecb2
LAU Sze Ting (230268178) <230268178@stu.vtc.edu.hk>
2024-05-29 11:18:50 +0800

    format dataGridView at Invoice Generate Delivery

--- Elysia/Elysia/InvoiceGenerateAndDelivery.Designer.cs
+++ Elysia/Elysia/InvoiceGenerateAndDelivery.Designer.cs
@@ -28,18 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label4 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btnViewInvoice = new System.Windows.Forms.RadioButton();
+            this.btnInvGen = new System.Windows.Forms.RadioButton();
+            this.btnLateDelivery = new System.Windows.Forms.RadioButton();
             this.btnUser = new System.Windows.Forms.RadioButton();
             this.btnLogout = new System.Windows.Forms.RadioButton();
             this.lblDept = new System.Windows.Forms.Label();
             this.imgLogo = new System.Windows.Forms.PictureBox();
             this.dgvOrder = new System.Windows.Forms.DataGridView();
             this.btnFilter = new System.Windows.Forms.Button();
-            this.btnLateDelivery = new System.Windows.Forms.RadioButton();
-            this.btnInvGen = new System.Windows.Forms.RadioButton();
-            this.btnViewInvoice = new System.Windows.Forms.RadioButton();
             this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.imgLogo)).BeginInit();
@@ -80,11 +82,68 @@
             this.panel1.Controls.Add(this.imgLogo);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Padding = new System.Windows.Forms.Padding(4);
             this.panel1.Size = new System.Drawing.Size(419, 1172);
             this.panel1.TabIndex = 30;
+            // 
+            // btnViewInvoice
+            // 
+            this.btnViewInvoice.Appearance = System.Windows.Forms.Appearance.Button;
+            this.btnViewInvoice.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
+            this.btnViewInvoice.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnViewInvoice.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnViewInvoice.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.btnViewInvoice.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnViewInvoice.Location = new System.Drawing.Point(4, 552);
+            this.btnViewInvoice.Margin = new System.Windows.Forms.Padding(4);
+            this.btnViewInvoice.Name = "btnViewInvoice";
+            this.btnViewInvoice.Size = new System.Drawing.Size(409, 83);
+            this.btnViewInvoice.TabIndex = 10;
+            this.btnViewInvoice.TabStop = true;
+            this.btnViewInvoice.Text = "View Invoice";
+            this.btnViewInvoice.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnViewInvoice.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnViewInvoice.UseVisualStyleBackColor = false;
+            // 
+            // btnInvGen
+            // 
+            this.btnInvGen.Appearance = System.Windows.Forms.Appearance.Button;
+            this.btnInvGen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
+            this.btnInvGen.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnInvGen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInvGen.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.btnInvGen.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnInvGen.Location = new System.Drawing.Point(4, 469);
+            this.btnInvGen.Margin = new System.Windows.Forms.Padding(4);
+            this.btnInvGen.Name = "btnInvGen";
+            this.btnInvGen.Size = new System.Drawing.Size(409, 83);
+            this.btnInvGen.TabIndex = 9;
+            this.btnInvGen.TabStop = true;
+            this.btnInvGen.Text = "Invoice Generate";
+            this.btnInvGen.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnInvGen.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnInvGen.UseVisualStyleBackColor = false;
+            // 
+            // btnLateDelivery
+            // 
+            this.btnLateDelivery.Appearance = System.Windows.Forms.Appearance.Button;
+            this.btnLateDelivery.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
+            this.btnLateDelivery.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLateDelivery.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLateDelivery.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.btnLateDelivery.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnLateDelivery.Location = new System.Drawing.Point(4, 386);
+            this.btnLateDelivery.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLateDelivery.Name = "btnLateDelivery";
+            this.btnLateDelivery.Size = new System.Drawing.Size(409, 83);
+            this.btnLateDelivery.TabIndex = 8;
+            this.btnLateDelivery.TabStop = true;
+            this.btnLateDelivery.Text = "Late Delivery";
+            this.btnLateDelivery.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnLateDelivery.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnLateDelivery.UseVisualStyleBackColor = false;
             // 
             // btnUser
             // 
@@ -95,7 +154,7 @@
             this.btnUser.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
             this.btnUser.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnUser.Location = new System.Drawing.Point(4, 303);
-            this.btnUser.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnUser.Margin = new System.Windows.Forms.Padding(4);
             this.btnUser.Name = "btnUser";
             this.btnUser.Size = new System.Drawing.Size(409, 83);
             this.btnUser.TabIndex = 7;
@@ -114,7 +173,7 @@
             this.btnLogout.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
             this.btnLogout.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnLogout.Location = new System.Drawing.Point(4, 1101);
-            this.btnLogout.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnLogout.Margin = new System.Windows.Forms.Padding(4);
             this.btnLogout.Name = "btnLogout";
             this.btnLogout.Size = new System.Drawing.Size(409, 65);
             this.btnLogout.TabIndex = 6;
@@ -142,7 +201,7 @@
             this.imgLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.imgLogo.Dock = System.Windows.Forms.DockStyle.Top;
             this.imgLogo.Location = new System.Drawing.Point(4, 4);
-            this.imgLogo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.imgLogo.Margin = new System.Windows.Forms.Padding(4);
             this.imgLogo.Name = "imgLogo";
             this.imgLogo.Size = new System.Drawing.Size(409, 216);
             this.imgLogo.TabIndex = 0;
@@ -150,7 +209,23 @@
             // 
             // dgvOrder
             // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvOrder.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dgvOrder.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvOrder.DefaultCellStyle = dataGridViewCellStyle2;
             this.dgvOrder.Location = new System.Drawing.Point(428, 119);
             this.dgvOrder.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dgvOrder.Name = "dgvOrder";
@@ -158,6 +233,7 @@
             this.dgvOrder.RowTemplate.Height = 27;
             this.dgvOrder.Size = new System.Drawing.Size(1482, 967);
             this.dgvOrder.TabIndex = 32;
+            this.dgvOrder.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dgvOrder_CellFormatting);
             // 
             // btnFilter
             // 
@@ -170,63 +246,6 @@
             this.btnFilter.Text = "Filter";
             this.btnFilter.UseVisualStyleBackColor = true;
             this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
-            // 
-            // btnLateDelivery
-            // 
-            this.btnLateDelivery.Appearance = System.Windows.Forms.Appearance.Button;
-            this.btnLateDelivery.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
-            this.btnLateDelivery.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnLateDelivery.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnLateDelivery.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
-            this.btnLateDelivery.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnLateDelivery.Location = new System.Drawing.Point(4, 386);
-            this.btnLateDelivery.Margin = new System.Windows.Forms.Padding(4);
-            this.btnLateDelivery.Name = "btnLateDelivery";
-            this.btnLateDelivery.Size = new System.Drawing.Size(409, 83);
-            this.btnLateDelivery.TabIndex = 8;
-            this.btnLateDelivery.TabStop = true;
-            this.btnLateDelivery.Text = "Late Delivery";
-            this.btnLateDelivery.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.btnLateDelivery.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.btnLateDelivery.UseVisualStyleBackColor = false;
-            // 
-            // btnInvGen
-            // 
-            this.btnInvGen.Appearance = System.Windows.Forms.Appearance.Button;
-            this.btnInvGen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
-            this.btnInvGen.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnInvGen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnInvGen.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
-            this.btnInvGen.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnInvGen.Location = new System.Drawing.Point(4, 469);
-            this.btnInvGen.Margin = new System.Windows.Forms.Padding(4);
-            this.btnInvGen.Name = "btnInvGen";
-            this.btnInvGen.Size = new System.Drawing.Size(409, 83);
-            this.btnInvGen.TabIndex = 9;
-            this.btnInvGen.TabStop = true;
-            this.btnInvGen.Text = "Invoice Generate";
-            this.btnInvGen.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.btnInvGen.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.btnInvGen.UseVisualStyleBackColor = false;
-            // 
-            // btnViewInvoice
-            // 
-            this.btnViewInvoice.Appearance = System.Windows.Forms.Appearance.Button;
-            this.btnViewInvoice.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(135)))), ((int)(((byte)(167)))));
-            this.btnViewInvoice.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnViewInvoice.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnViewInvoice.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
-            this.btnViewInvoice.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnViewInvoice.Location = new System.Drawing.Point(4, 552);
-            this.btnViewInvoice.Margin = new System.Windows.Forms.Padding(4);
-            this.btnViewInvoice.Name = "btnViewInvoice";
-            this.btnViewInvoice.Size = new System.Drawing.Size(409, 83);
-            this.btnViewInvoice.TabIndex = 10;
-            this.btnViewInvoice.TabStop = true;
-            this.btnViewInvoice.Text = "View Invoice";
-            this.btnViewInvoice.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.btnViewInvoice.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.btnViewInvoice.UseVisualStyleBackColor = false;
             // 
             // InvoiceGenerateAndDelivery
             // 
